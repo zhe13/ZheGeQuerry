243bc991e8fc3b30aec7953ef72c326322e82aff
zhe13 <lovewutianzhe@vip.qq.com>
2015-09-04 17:57:08 +0800

    change raffling model

--- Raffle/raffle.js
+++ Raffle/raffle.js
@@ -1,7 +1,15 @@
+//date   :2015-09-04
+//		author :zhe13
+//		email  :wutianzhe123@gmail.com
+//		name   :抽奖系统
+//		fix	   :加入了保底奖励措施
+//
+//
 $(document).ready(function(){ 
 	    //-----------global variables-------------
 		var raffleTime = 0;
 		var arr = new Array;
+		var fix = new Array;
 		var jsonPath = confirm("金币抽取点确定，水晶点取消")?"./listCoins.json":"./listJewelry.json";
 		//-----------global variables-------------
 		$.getJSON(jsonPath,function(data){ 
@@ -21,16 +29,19 @@ $(document).ready(function(){
 		}) 
 		
 		$("#btn1").click(function(){
-			getItem();
+			getItem1();
 		})
 		$("#btn2").click(function(){
+			raffleTime=0;
 			for(var i=0;i<100;i++){
 			///////////////自动一百次/////////////////////
-			getItem();	
+			getItem1();	
 			}
 			raffleTime=0;		
 		})
-
+		$("#btn3").click(function(){
+			getItem2();
+		})
 		
 		//获取权重总和的函数
 		function getSumWeight(num){
@@ -43,7 +54,7 @@ $(document).ready(function(){
 		}
 		
 		//抽取方法1，权重＋区间
-		function getItem(){
+		function getItem1(){
 			if(raffleTime ===0){$("#jsonTip").empty();var raffle ;}
 			if(raffleTime<25){
 				raffle = Math.random()*getSumWeight(22);			
@@ -59,6 +70,39 @@ $(document).ready(function(){
 						raffle-=parseFloat(arr[x]["weight"]);
 					}
 				}
+			raffleTime++;
+		}
+		
+		function getItem2(){
+			//初始化fix数组
+			if(raffleTime ===0){
+				$("#jsonTip").empty();
+				var raffle ;
+				for(var x in arr){
+					if(arr[x]["fix"]){
+						fix[(arr[x]["fix"])]=x;//这边其实不用减去一，因为后续
+					}
+				}
+				console.log(fix,arr,typeof fix[1]);//修正数组
+			}
+			console.log(typeof fix[raffleTime]);
+			if(typeof fix[raffleTime] != "undefined"){
+				raffle = getSumWeight(fix[raffleTime]);
+			}			
+			else if(raffleTime<25){
+				raffle = Math.random()*getSumWeight(22);			
+			}else{
+				raffle = Math.random()*getSumWeight(arr.length);
+			}
+			
+			for(var x in arr){
+				if(raffle<=0){	
+					$("#jsonTip").append("<br/>第"+raffleTime+"次&nbsp;&nbsp;&nbsp;"+arr[x]["item"],"&nbsp;&nbsp;&nbsp;"+arr[x]["price"]);//用append追加内容
+					break;//&nbsp;空格转义
+				}else{
+					raffle-=parseFloat(arr[x]["weight"]);
+				}
+			}
 			raffleTime++;
 		}
 				
